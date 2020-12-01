d87e18e9af2c820113cee849ea1106bc5373f62b
julian <juliancano.dev@gmail.com>
2020-12-01 16:17:40 -0500

    Task 0

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -10,13 +10,13 @@ class Program
         {
             Console.WriteLine($"{number} is positive");
         }
-        else if (number = 0)
-        {
-            Console.WriteLine($"{number} is zero");
-        }
         else if (number < 0)
         {
             Console.WriteLine($"{number} is negative");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is zero");
         }
     }
 }

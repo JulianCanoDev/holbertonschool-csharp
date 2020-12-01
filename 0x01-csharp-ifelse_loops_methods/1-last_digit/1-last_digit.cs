51ba2850a6b2b7dce059a7b138588779aad64b6a
julian <juliancano.dev@gmail.com>
2020-12-01 17:33:55 -0500

    Task 1

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace _1_last_digit
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+        if (lastDigit > 5)
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is greater than 5");
+        else if (lastDigit % 10 == 0)
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is 0");
+        else if (lastDigit < 6 && lastDigit != 0)
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is less than 6 and not 0");
     }
 }

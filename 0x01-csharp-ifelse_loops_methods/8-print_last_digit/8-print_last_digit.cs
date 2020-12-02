a035190f61ebf76b670eba41bb100a6935c624e3
julian <juliancano.dev@gmail.com>
2020-12-01 21:46:38 -0500

    Task 8

--- 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -4,6 +4,15 @@ class Number
 {
     public static int PrintLastDigit(int number)
     {
-        return(number % 10);
+        if (number < 0)
+        {
+            Console.Write((number % 10) * -1);
+            return((number % 10) * -1);
+        }
+        else
+        {
+            Console.Write(number % 10);
+            return(number % 10);
+        }
     }
 }

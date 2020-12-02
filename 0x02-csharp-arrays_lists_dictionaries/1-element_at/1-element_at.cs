9fe6d0e9b45ea42608a1868f80e477f84f90060c
julian <juliancano.dev@gmail.com>
2020-12-02 18:35:05 -0500

    Task 0

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -6,7 +6,7 @@ class Array
     {
         if (index < 0 || index >= array.Length)
         {
-            Console.Write("Index out of range");
+            Console.WriteLine("Index out of range");
             return(1);
         }
         return(array[index]);

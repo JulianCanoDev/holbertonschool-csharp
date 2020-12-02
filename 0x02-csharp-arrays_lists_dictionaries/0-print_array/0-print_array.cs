c3c81272bcb3382e709c4f616a8e5c9a7435aaf1
julian <juliancano.dev@gmail.com>
2020-12-02 18:44:24 -0500

    Task 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,6 +4,7 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
+        int i;
         if (size == 0)
         {
             Console.Write('\n');
@@ -13,5 +14,15 @@ class Array
             Console.WriteLine("Size cannot be negative");
             return (null);
         }
+        int[] new_array = new int[size];
+        for (i = 0 ; i < new_array.Length ; i++)
+        {
+            new_array[i] = i;
+            if (i < new_array.Length - 1)
+                Console.Write(new_array[i] + " ");
+            else
+                Console.WriteLine(new_array[i]);
+        }
+        return(new_array);
     }
 }

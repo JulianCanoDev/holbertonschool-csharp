163f7a6c9ab20b85bdedb9466112381b718b71d8
julian <juliancano.dev@gmail.com>
2020-12-01 19:21:01 -0500

    Task 6

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,29 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i;
+            int j;
+            int counter = 0;
+            for (i = 0 ; i <= 9 ; i++)
+            {
+                counter++;
+                for (j = i + 0 ; j <= 9 ; j++)
+                {
+                    if (j < counter)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write($"{i}{j}");
+                        if (i != 8 || j != 9)
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                }
+            }
+            Console.Write("\n");
         }
     }
 }

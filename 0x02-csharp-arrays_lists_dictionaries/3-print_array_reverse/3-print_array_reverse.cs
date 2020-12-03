9f63ffe186f85c0ed5d5b1e90e96492c5e895cfc
julian <juliancano.dev@gmail.com>
2020-12-02 20:20:15 -0500

    Task 3

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,6 +4,16 @@ class Array
 {
     public static void Reverse(int[] array)
     {
-        Console.WriteLine("Hello World!");
+        int i;
+        if (array != null)
+        {
+            for (i = array.Length - 1 ; i >= 0 ; i--)
+            {
+                if (array > 0)
+                    Console.Write($"{array[i]} ");
+                else
+                    Console.WriteLine(array[i]);
+            }
+        }
     }
 }

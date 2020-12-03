d1058aafac16243b8f520142f0fb7f8777299b86
julian <juliancano.dev@gmail.com>
2020-12-02 23:28:27 -0500

    Task 6

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        Console.WriteLine("Hello World!");
+        int i;
+        int max_value;
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return(-1);
+        }
+        for (i = 0 ; i <= myList.Count ; i++)
+        {
+            if (myList[i] > myList[i + 1])
+                max_value = myList[i];
+        }
+        return(max_value);
     }
 }
--- 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class List
 {
     public static List<bool> DivisibleBy2(List<int> myList)
     {
-        Console.WriteLine("Hello World!");
+        List<bool> resultList = new List<bool>();
+        for (int pos = 0; pos < myList.Count; pos++)
+        {
+            if (myList[pos] % 2 == 0)
+            {
+                resultList.Add(true);
+            }
+            else
+            {
+                resultList.Add(false);
+            }
+        }
+        return resultList;
     }
 }

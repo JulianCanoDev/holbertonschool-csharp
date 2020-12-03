2c2c85de4f9fe3263f7c26b204632df3589f2e91
julian <juliancano.dev@gmail.com>
2020-12-02 23:48:56 -0500

    Task 5

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,17 +5,17 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
-        int i;
         int max_value;
         if (myList.Count == 0)
         {
             Console.WriteLine("List is empty");
             return(-1);
         }
-        for (i = 0 ; i <= myList.Count ; i++)
+        max_value = myList[0];
+        foreach (var number in myList)
         {
-            if (myList[i] > myList[i + 1])
-                max_value = myList[i];
+            if (number > max_value)
+                max_value = number;
         }
         return(max_value);
     }

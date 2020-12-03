4bfc10bbe6351c3430ba342aeb9ca14838f0f9e8
julian <juliancano.dev@gmail.com>
2020-12-03 16:19:24 -0500

    Readme

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        Console.WriteLine("Hello World!");
+        var new_list = new List<int>();
+        int i, j;
+        var list1_lengh = list1.Count;
+        var list2_lengh = list2.Count;
+
+        for (i = 0 ; i < list1_lengh ; i++)
+        {
+            for (j = 0 ; j < list2_lengh ; j++)
+            {
+                if (list1[i] != list2[j])
+                    new_list.Add(list1[i]);
+            }
+        }
+        return(new_list);
     }
 }

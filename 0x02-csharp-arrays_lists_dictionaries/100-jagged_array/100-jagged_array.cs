205ece0ce93297f9bc44a121f209ad126b29ec5e
julian <juliancano.dev@gmail.com>
2020-12-02 22:00:12 -0500

    Task 100

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -6,7 +6,24 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i, j;
+            int[][] jagged = new int[3][]
+            {
+                new int[] {0, 1, 2, 3},
+                new int[] {0, 1, 2, 3, 4, 5, 6},
+                new int[] {0, 1}
+            };
+
+            for (i = 0 ; i < 3 ; i++)
+            {
+                for (j = 0 ; j < jagged[i].Length ; j++)
+                {
+                    Console.Write(jagged[i][j]);
+                    if (j < jagged[i].Length - 1)
+                        Console.Write(" ");
+                }
+                Console.Write('\n');
+            }
         }
     }
 }
--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -4,6 +4,22 @@ class List
 {
     public static List<int> CreatePrint(int size)
     {
-        Console.WriteLine("Hello World!");
+        int i;
+        List<int> new_list = new List<int>();
+        int counter = 0;
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        for (i = 0 ; i <= size ; i++)
+        {
+            new_list[i] = counter;
+            if (i < new_list.Length - 1)
+                Console.Write(new_list[i] + " ");
+            else
+                Console.WriteLine(new_list[i]);
+        }
+        return(new_list);
     }
 }

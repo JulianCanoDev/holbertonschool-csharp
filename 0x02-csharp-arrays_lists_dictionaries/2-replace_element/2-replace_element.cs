69237325a63603b93f14c30ce75a53c31fea10bf
julian <juliancano.dev@gmail.com>
2020-12-02 19:38:51 -0500

    Task 2

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,6 +4,12 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        Console.WriteLine("Hello World!");
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return(array);
+        }
+        array[index] = n;
+        return(array);
     }
 }

be4b129646d8a1433bbcc653ad2535ef21dcd244
julian <juliancano.dev@gmail.com>
2020-12-03 17:38:28 -0500

    Task 2

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -6,18 +6,39 @@ class List
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
         var new_list = new List<int>();
-        int i, j;
+        int i, j, same;
         var list1_lengh = list1.Count;
         var list2_lengh = list2.Count;
 
         for (i = 0 ; i < list1_lengh ; i++)
         {
+            same = 0;
             for (j = 0 ; j < list2_lengh ; j++)
             {
-                if (list1[i] != list2[j])
-                    new_list.Add(list1[i]);
+                if (list1[i] == list2[j])
+                {
+                    same = 1;
+                    break;
+                }
             }
+            if (same == 0)
+                new_list.Add(list1[i]);
         }
+        for (i = 0 ; i < list2_lengh ; i++)
+        {
+            same = 0;
+            for (j = 0 ; j < list1_lengh ; j++)
+            {
+                if (list2[i] == list1[j])
+                {
+                    same = 1;
+                    break;
+                }
+            }
+            if (same == 0)
+                new_list.Add(list2[i]);
+        }
+        new_list.Sort();
         return(new_list);
     }
 }

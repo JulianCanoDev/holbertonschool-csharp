69b7df9e60f5890de40f0d736645beaf9a4d7fd8
julian <juliancano.dev@gmail.com>
2020-12-02 22:25:14 -0500

    Task 4

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class List
 {
@@ -6,20 +7,20 @@ class List
     {
         int i;
         List<int> new_list = new List<int>();
-        int counter = 0;
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return (null);
         }
-        for (i = 0 ; i <= size ; i++)
+        for (i = 0 ; i <= size - 1 ; i++)
         {
-            new_list[i] = counter;
-            if (i < new_list.Length - 1)
-                Console.Write(new_list[i] + " ");
+            new_list.Add(i);
+            if (i < new_list.Count - 1)
+                Console.Write(new_list[i]);
             else
-                Console.WriteLine(new_list[i]);
+                Console.Write($"{new_list[i]} ");
         }
+        Console.Write('\n');
         return(new_list);
     }
 }

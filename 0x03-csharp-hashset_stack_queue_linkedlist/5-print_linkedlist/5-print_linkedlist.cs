c71ee341fedf276517199fe33457c4ae22ff3805
julian <juliancano.dev@gmail.com>
2020-12-03 19:21:29 -0500

    Task 5

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class MyQueue
+class LList
 {
-    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    public static LinkedList<int> CreatePrint(int size)
     {
-        Console.WriteLine("Hello World!");
+        int i;
+        LinkedList<int> list = new LinkedList<int>();
+
+        if (size < 0)
+            return(list);
+
+        for (i = 0 ; i < size ; i++)
+        {
+            Console.WriteLine(i);
+            list.AddLast(i);
+        }
+        return(list);
     }
 }

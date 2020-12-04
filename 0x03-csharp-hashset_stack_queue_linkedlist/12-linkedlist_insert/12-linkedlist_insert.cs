c7c6a1dfce7205236878a5e449c6b3a153dc1ee6
julian <juliancano.dev@gmail.com>
2020-12-03 21:20:40 -0500

    Task 9, 10, 11

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,6 +5,23 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        Console.WriteLine("Hello World!");
+        if (n < myLList.First.Value)
+            myLList.AddFirst(n);
+
+        else if (n > myLList.Last.Value)
+            myLList.AddLast(n);
+
+        else
+        {
+            foreach (int element in myLList)
+            {
+                if (element > n)
+                {
+                    myLList.AddBefore(myLList.Find(element), n);
+                    break;
+                }
+            }
+        }
+        return (myLList.Find(n));
     }
 }

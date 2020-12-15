20e66b4c9f5ff8df8decb107a9fe5d6fbe16ae11
julian <juliancano.dev@gmail.com>
2020-12-15 14:35:10 -0500

    Task 1

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -8,9 +8,6 @@ namespace Enemies
     class Zombie
     {
         public int health;
-        /// <summary>
-        /// This is our class Zombie.
-        /// </summary>
         public Zombie(int health = 0)
         {
             this.health = health;

c39f529010a62ac65a547e958e96ca7ff7c46bbe
julian <juliancano.dev@gmail.com>
2020-12-15 15:26:47 -0500

    Task 2

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace _2_enemy
+namespace Enemies
 {
-    class Program
+    /// <summary>
+    /// This is our class Zombie.
+    /// </summary>
+    class Zombie
     {
-        static void Main(string[] args)
+        public int health;
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this.health = value;
+            }
+            else if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
         }
     }
 }

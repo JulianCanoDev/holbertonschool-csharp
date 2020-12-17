8c082c29e99c3318ca97a416942bcc350de4a8f3
julian <juliancano.dev@gmail.com>
2020-12-17 10:54:39 -0500

    Task 0

--- 0x07-csharp-tdd/0-add/MyMath.Test/MyMath.Test.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Test/MyMath.Test.cs
@@ -1,18 +1,15 @@
 using NUnit.Framework;
+using MyMath;
 
 namespace MyMath.Test
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void Test_Add_20_from_12_8()
         {
-            Assert.Pass();
+            Assert.AreEqual(20, Operations.Add(12, 8));
         }
     }
 }
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,7 +2,17 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// This is our class Operators.
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// This is our class public static int Add.
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return(a + b);
+        }
     }
 }

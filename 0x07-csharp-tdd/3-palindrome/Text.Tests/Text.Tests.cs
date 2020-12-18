8d41be181f7e7a42eb06cc54f1726233b15952f5
julian <juliancano.dev@gmail.com>
2020-12-17 23:01:58 -0500

    Task 3

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -1,18 +1,27 @@
 using NUnit.Framework;
+using Text;
 
 namespace Text.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
-        public void Test1()
+        public void Test_Palindrome_01()
         {
-            Assert.Pass();
+            Assert.AreEqual(true, Str.IsPalindrome(""));
+        }
+        public void Test_Palindrome_02()
+        {
+            Assert.AreEqual(true, Str.IsPalindrome("reconocer"));
+        }
+        public void Test_Palindrome_03()
+        {
+            Assert.AreEqual(false, Str.IsPalindrome("hello"));
+        }
+        public void Test_Palindrome_04()
+        {
+            Assert.AreEqual(true, Str.IsPalindrome("A man, a plan, a canal: Panama."));
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,32 @@
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// This is our class Matrix.
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// This is our class Matrix.
+        /// </summary>
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length == 0)
+                return(true);
+
+            string str = s;
+            str = str.ToLower();
+            str = str.Trim(' ', ',', '.', ':', ';');
+
+            int i = 0;
+            int j = str.Length - 1;
+
+            for ( ; i < j ; i++, j--)
+            {
+                if (str[i] != str[j])
+                    return(false);
+            }
+            return(true);
+        }
     }
 }

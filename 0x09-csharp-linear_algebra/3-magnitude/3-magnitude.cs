58f748f4f704c26f1100a8a04ab42652523bb738
julian <juliancano.dev@gmail.com>
2021-01-16 14:33:50 -0500

    Task 4

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,6 +4,12 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
-        Console.WriteLine("Hello World!");
+        if (vector.Length != 2 || vector.Length != 3)
+            return(-1);
+
+        if (vector.Length == 2)
+            return(Math.Sqrt(Math.Pow(vector[0] + vector[1], 2)));
+        if (vector.Length == 3)
+            return(Math.Sqrt(Math.Pow(vector[0] + vector[1] + vector[2], 2)));
     }
 }

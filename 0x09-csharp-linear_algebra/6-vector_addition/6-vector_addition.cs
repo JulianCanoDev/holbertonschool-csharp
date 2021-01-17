6aaeb4a539813a056bd65e8abcc364bb04656df1
julian <juliancano.dev@gmail.com>
2021-01-17 15:08:24 -0500

    Task 10

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -8,11 +8,12 @@ class VectorMath
     public static double[] Add(double[] vector1, double[] vector2)
     {
         double[] sum = new double[2];
+        int i;
 
         if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
             || vector2.Length < 2 || vector2.Length > 3)
         {
-        for (int i = 0; i < 2; i++)
+        for (i = 0 ; i < 2 ; i++)
             sum[i] = -1;
         return sum;
         }
@@ -24,7 +25,9 @@ class VectorMath
             len = 3;
 
         double[] sum2 = new double[len];
-        for (int i = 0; i < vector1.Length; i++)
+        int vector1_lenght = vector1.Length;
+
+        for (i = 0 ; i < vector1_lenght ; i++)
             sum2[i] = (vector1[i] + vector2[i]);
 
         return sum2;

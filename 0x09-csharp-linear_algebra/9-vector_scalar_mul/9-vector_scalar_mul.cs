08d8419805802100ec792f4a84fc046bb901de87
julian <juliancano.dev@gmail.com>
2021-01-17 14:22:45 -0500

    Task 10

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -4,6 +4,27 @@ class VectorMath
 {
     public static double[] Multiply(double[] vector, double scalar)
     {
-        Console.WriteLine("Hello World!");
+        double[] mul = new double[2];
+        int i;
+
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            for (i = 0 ; i < 2 ; i++)
+                mul[i] = -1;
+            return mul;
+        }
+
+        int len;
+        if  (vector.Length == 2)
+            len = 2;
+        else
+            len = 3;
+
+        int vector_lenght = vector.Length;
+        double[] mul2 = new double[len];
+        for (i = 0 ; i < vector_lenght ; i++)
+            mul2[i] = (vector[i] * scalar);
+
+        return mul2;
     }
 }

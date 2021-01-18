4730be7ad716c4934815d3505e665e0308bd1c8a
julian <juliancano.dev@gmail.com>
2021-01-18 15:07:43 -0500

    Task 12

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -6,9 +6,6 @@ class VectorMath
     /// <summary>This is the class object.</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        double sum = 0;
-        int i;
-
         if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
             || vector2.Length < 2 || vector2.Length > 3)
         {
@@ -16,6 +13,8 @@ class VectorMath
         }
 
         int vector1_lenght = vector1.Length;
+        double sum = 0;
+        int i;
 
         for (i = 0 ; i < vector1_lenght ; i++)
             sum += (vector1[i] * vector2[i]);

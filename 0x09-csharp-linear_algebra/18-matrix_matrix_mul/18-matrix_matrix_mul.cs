698d9044b4a6c582590727fa77aa32532ba48243
julian <juliancano.dev@gmail.com>
2021-01-19 16:38:40 -0500

    Task 19

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -7,10 +7,8 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         if (
-            (matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3) ||
-            (matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3) ||
-            (matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3) ||
-            (matrix2.GetLength(1) < 2 || matrix2.GetLength(1) > 3)
+            (!(matrix1 is double[,]) || !(matrix2 is double[,])) ||
+            (matrix2.GetLength(0) != matrix1.GetLength(1))
         )
         {
             return new double[,]{{-1}};

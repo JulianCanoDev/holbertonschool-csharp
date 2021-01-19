af99c3cc2920791ff0256f3ab4191d81663a80c7
julian <juliancano.dev@gmail.com>
2021-01-19 10:54:58 -0500

    Task 17

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -6,23 +6,25 @@ class MatrixMath
     /// <summary>This is the class object.</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if (matrix is double[,] && (2 == matrix.GetLength(0) && 2 == matrix.GetLength(1)) ||
-        (3 == matrix.GetLength(0) && 3 == matrix.GetLength(1)) )
+        if (
+            (matrix.GetLength(0) != matrix.GetLength(1)) ||
+            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
+            (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+        )
         {
-            int i, j;
-            int matrix_len = matrix.GetLength(0);
-
-            for (i = 0 ; i < matrix_len ; i++)
-            {
-                for (j = 0 ; j < matrix_len ; j++)
-                {
-                    matrix[i, j] += (matrix[i, j] * scalar);
-                }
-            }
-
-            return(matrix);
+            return new double[,]{{-1}};
         }
 
-        else return new double[,]{{-1}};
+        int dimMat = matrix.GetLength(0);
+        double[,] kMatrix = new double[dimMat, dimMat];
+
+        for (int vec = 0; vec < dimMat; vec++)
+        {
+            for (int row = 0; row < dimMat; row++)
+            {
+                kMatrix[vec, row] = scalar * matrix[vec, row] ;
+            }
+        }
+        return kMatrix;
     }
 }

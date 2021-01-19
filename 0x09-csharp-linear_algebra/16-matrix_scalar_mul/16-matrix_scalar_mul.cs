0df93784b6c1d05cfc61f642fd7c995c4862f11f
julian <juliancano.dev@gmail.com>
2021-01-19 10:47:48 -0500

    Task 17

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -7,10 +7,9 @@ class MatrixMath
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
         if (
-            (!(matrix is double[,])) ||
             (matrix.GetLength(0) != matrix.GetLength(1)) ||
-            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
-            (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+            (2 != matrix.GetLength(0) && 2 != matrix.GetLength(1)) ||
+            (3 != matrix.GetLength(0) && 3 != matrix.GetLength(1))
         )
         {
             return new double[,]{{-1}};

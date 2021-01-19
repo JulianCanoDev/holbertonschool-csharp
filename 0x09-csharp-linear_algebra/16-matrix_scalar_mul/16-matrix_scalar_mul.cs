316d62257f95fd3fb2b2c734b6936996551d56fb
julian <juliancano.dev@gmail.com>
2021-01-19 10:00:48 -0500

    Task 17

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,9 +1,31 @@
 ﻿using System;
 
+/// <summary>This is the class object.</summary>
 class MatrixMath
 {
+    /// <summary>This is the class object.</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        Console.WriteLine("Hello World!");
+        if (
+            (matrix.GetLength(0) != matrix.GetLength(1)) ||
+            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
+            (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+        )
+        {
+            return new double[,]{{-1}};
+        }
+
+        int i, j;
+        int matrix_len = matrix.GetLength(0);
+
+        for (i = 0 ; i < matrix_len ; i++)
+        {
+            for (j = 0 ; j < matrix_len ; j++)
+            {
+                matrix[i, j] += (matrix[i, j] * scalar);
+            }
+        }
+
+        return(matrix);
     }
 }

70a51e44a68020040490b87efc057e5c76575f77
julian <juliancano.dev@gmail.com>
2021-01-21 13:02:19 -0500

    Task 28

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -16,7 +16,7 @@ class MatrixMath
         {
             detMatrix = Math.Round(
                 (
-                    matrix[0, 0]*matrix[1, 1]*matrix[2, 2] + 
+                    matrix[0, 0]*matrix[1, 1]*matrix[2, 2] +
                     matrix[0, 1]*matrix[1, 2]*matrix[2, 0] +
                     matrix[0, 2]*matrix[1, 0]*matrix[2, 1]
                 ) -
@@ -26,7 +26,7 @@ class MatrixMath
                     matrix[0, 2]*matrix[1, 1]*matrix[2, 0]
                 ), 2);
         } else { return -1; }
-        
+
         return detMatrix;
     }
 }

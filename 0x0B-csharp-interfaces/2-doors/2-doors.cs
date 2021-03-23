d9cdc94fa7b8b6794fafd91a49ab94550e266baa
Julian Cano VR <juliancano.dev@gmail.com>
2021-03-23 17:19:04 -0500

    Task 1

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -33,37 +33,12 @@ public interface IBreakable
     void Break();
 }
 
-interface ICollectable
+/// <summary>This is Interact.</summary>
+public interface ICollectable
 {
+    /// <summary>This is Interact.</summary>
     bool isCollected{ get; set; }
-
+    /// <summary>This is Interact.</summary>
     void Collect();
 }
 
-/// <summary>
-/// This is Queue.
-/// </summary>
-public class TestObject : Base, IInteractive, IBreakable, ICollectable
-{
-    /// <summary>This is durability implemented int.</summary>
-    public int durability{ get => throw new NotImplementedException() ; set => throw new NotImplementedException(); }
-    /// <summary>This is isCollected implemented bool.</summary>
-    public bool isCollected{ get => throw new NotImplementedException() ; set => throw new NotImplementedException(); }
-    /// <summary>This is Interact.</summary>
-    public void Interact()
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>This is break.</summary>
-    public void Break()
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>This is Collect.</summary>
-    public void Collect()
-    {
-        throw new NotImplementedException();
-    }
-}

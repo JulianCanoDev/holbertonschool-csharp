22b6ccf0384ba5dc33b79d007d12a9963d729a70
Julian Cano VR <juliancano.dev@gmail.com>
2021-03-31 16:38:32 -0500

    Structure

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace InventoryLibrary
+{
+    /// <summary>
+    /// This is the base class for the inventory management.
+    /// </summary>
+    public class Inventory : BaseClass
+    {
+        
+    }
+}

b8478f71c09726aae9c4a03bf354f709b60c98fa
Julian Cano VR <juliancano.dev@gmail.com>
2021-04-04 10:38:26 -0500

    Task 3

---
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace InventoryManager
+{
+    class Program
+    {
+        
+    }
+}
